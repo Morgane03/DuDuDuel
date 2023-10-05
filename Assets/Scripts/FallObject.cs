39e649348f54313eaf0db5fe24f982a36a60332d
Eva291204 <eva.fricaud@e-artsup.net>
2023-10-05 16:59:13 +0200

    objet qui tombe

--- Assets/Scripts/FallObject.cs
+++ Assets/Scripts/FallObject.cs
@@ -5,12 +5,8 @@ using UnityEngine;
 
 public class FallObject : MonoBehaviour
 {
-    public Vector2 speed;
-    public Vector2 direction;
     public void Fall()
     {
-        Vector2 mouvement = new Vector2(speed.x * direction.x, speed.y * direction.y);
-        mouvement *= Time.deltaTime;
-        gameObject.transform.Translate(mouvement);
+        gameObject.GetComponent<Rigidbody>().useGravity = true;
     }
 }
