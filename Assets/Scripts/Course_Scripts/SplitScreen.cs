0c45c8be5bf27177075df2b8710e98ac2b92a55c
m v <morgane.voisin@e-artsup.net>
2023-12-01 09:57:30 +0100

    Split sceen works again git add Assets/git add Assets/!

--- Assets/Scripts/Course_Scripts/SplitScreen.cs
+++ Assets/Scripts/Course_Scripts/SplitScreen.cs
@@ -25,25 +25,39 @@ public class SplitScreen : MonoBehaviour
     {
         _UICamera.gameObject.SetActive(false);
 
-        if(nbPlayer == 2)
+        Camera firstCamera = _camera[0];
+        Camera secondCamera = _camera[1];
+        Camera thirdCamera = _camera[2];
+        Camera fourthCamera = _camera[3];
+
+        if (nbPlayer == 2)
         {
-            Camera firstCamera = _camera[0];
-            Camera secondCamera = _camera[1];
             firstCamera.gameObject.SetActive(true);
             secondCamera.gameObject.SetActive(true);
             firstCamera.rect = new Rect(0, 0, 1f, 1);
             secondCamera.rect = new Rect(0, 0.5f, 1f, 1);
         }
-        else
+        else if(nbPlayer == 3)
         {
-            if(nbPlayer == 3)
-            {
-                _nullCamera.gameObject.SetActive(true);
-            }
-            for(int i = 0; i < nbPlayer; i++)
-            {
-                _camera[i].gameObject.SetActive(true);
-            }
+            firstCamera.gameObject.SetActive(true);
+            secondCamera.gameObject.SetActive(true);
+            thirdCamera.gameObject.SetActive(true);
+            _nullCamera.gameObject.SetActive(true);
+            firstCamera.rect = new Rect(0, 0, 0.5f, 0.5f);
+            secondCamera.rect = new Rect(0.5f, 0, 0.5f, 0.5f);
+            thirdCamera.rect = new Rect(0, 0.5f, 1f, 0.5f);
+            _nullCamera.rect = new Rect(0.5f, 0.5f, 0.5f, 0.5f);
+        }
+        else if(nbPlayer == 4)
+        {
+            firstCamera.gameObject.SetActive(true);
+            secondCamera.gameObject.SetActive(true);
+            thirdCamera.gameObject.SetActive(true);
+            fourthCamera.gameObject.SetActive(true);
+            firstCamera.rect = new Rect(0, 0, 0.5f, 0.5f);
+            secondCamera.rect = new Rect(0.5f, 0, 0.5f, 0.5f);
+            thirdCamera.rect = new Rect(0, 0.5f, 0.5f, 0.5f);
+            fourthCamera.rect = new Rect(0.5f, 0.5f, 0.5f, 0.5f);
         }
     }
 }
