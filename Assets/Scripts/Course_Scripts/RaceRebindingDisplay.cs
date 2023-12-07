5f66bee6eb9318268ac12cd5fe1491de93f402d1
m v <morgane.voisin@e-artsup.net>
2023-12-07 11:07:52 +0100

    Debut rebinding keys

---
+++ Assets/Scripts/Course_Scripts/RaceRebindingDisplay.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// made using this : https://www.youtube.com/watch?v=qXbjyzBlduY&t=799s
+public class RaceRebindingDisplay : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
