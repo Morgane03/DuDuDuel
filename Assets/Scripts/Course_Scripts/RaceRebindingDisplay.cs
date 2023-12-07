fa8c8141b4cf8f9775ce26d8a65daed96c482d80
m v <morgane.voisin@e-artsup.net>
2023-12-07 16:24:40 +0100

    Echec de la mission de remap

--- Assets/Scripts/Course_Scripts/RaceRebindingDisplay.cs
+++ Assets/Scripts/Course_Scripts/RaceRebindingDisplay.cs
@@ -1,20 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
 
 
 // made using this : https://www.youtube.com/watch?v=qXbjyzBlduY&t=799s
 public class RaceRebindingDisplay : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public InputActionRebindingExtensions.RebindingOperation rebindingOperation;
 
-    // Update is called once per frame
-    void Update()
+    public void RebindKeyboard(InputActionReference _ref)
     {
-        
+        InputAction _action = _ref.action;
+        _action.Disable();
+        rebindingOperation = _ref.action.PerformInteractiveRebinding(0)
+            .WithControlsExcluding("Mouse")
+            .WithControlsExcluding("Gamepad")
+            .WithControlsExcluding("Touchpad")
+            .OnMatchWaitForAnother(0.1f)
+            .OnComplete(operation => RebindComplete(_action))
+            .Start();
+    }
+    void RebindComplete(InputAction _action)
+    {
+        Debug.Log(rebindingOperation.action.bindings[0].effectivePath);
+        _action.Enable();
+        rebindingOperation.Dispose();
+        Debug.Log(InputControlPath.ToHumanReadableString(_action.bindings[0].effectivePath, InputControlPath.HumanReadableStringOptions.OmitDevice));
     }
 }
