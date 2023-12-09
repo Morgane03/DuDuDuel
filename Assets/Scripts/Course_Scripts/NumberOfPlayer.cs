aef6c9129e9f2299c709e4975e5520f57961a86d
m v <morgane.voisin@e-artsup.net>
2023-12-09 17:22:18 +0100

    delate using

--- Assets/Scripts/Course_Scripts/NumberOfPlayer.cs
+++ Assets/Scripts/Course_Scripts/NumberOfPlayer.cs
@@ -1,8 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Events;
-using UnityEngine.UI;
 
 // Lets you tell the splitscrenn script the number of players
 public class NumberOfPlayer : MonoBehaviour
